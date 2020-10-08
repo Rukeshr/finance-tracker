7996d548d57de95cba68f067e6687b36831f9d39
Rukesh R <Rukeshr16@gmail.com>
2020-10-08 13:29:58 +0530

    Finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
                       #current_user method is provided by devise gem
   end
@@ -8,10 +9,16 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
+  def show 
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
   def search
     if params[:friend].present?
-      @friend = params[:friend]
-      if @friend
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
         respond_to do |format|
           format.js { render partial: 'users/friend_result' }
         end

c72eee79ff4fd08fdd5a04730ebbef9f7f59f241
Rukesh R <Rukeshr16@gmail.com>
2020-10-08 10:55:29 +0530

    Added first & last name to login and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @tracked_stocks = current_user.stocks
+                      #current_user method is provided by devise gem
   end
 end
